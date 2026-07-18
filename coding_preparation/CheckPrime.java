35c7d66b4e9390b20f0fdc0d3c8bdcf8e692a136
Krish Mishra <rm25101980@gmail.com>
2026-07-18 18:00:54 +0530

    Revert "un-optimised code for checking prime"
    This reverts commit 5b079a7b7b02e04b91747ee7f7dd60fa6ccac814.

--- coding_preparation/CheckPrime.java
+++ coding_preparation/CheckPrime.java
@@ -3,7 +3,7 @@ package coding_preparation;
 public class CheckPrime {
     public static boolean isPrime(int n) {
         if (n <= 1) return false;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) return false;
         }
         return true;
